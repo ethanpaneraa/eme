ee80288a727da4ed085af8d0f6e2f2eb7e97a47e
Ethan Pineda <joshuapineda66@gmail.com>
2025-09-22 17:46:21 -0700

    maybe

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/groupme-vector/',
+  base: 'https://github.com/ethanpaneraa/eme/',
 });
